386c46456298e4ac022d637ff9c3adb75032d65f
soucoder9 <soubhadra1234@gmail.com>
2025-01-13 08:03:14 +0530

    initial commit

---
+++ C++/copyconstructors.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+class Coffee
+{
+public:
+    string Type;
+    int Servings;
+    vector<string> Ingridients;
+
+    Coffee(string name,int qty,vector<string>ing)//default constructor with parameters....
+    {
+        cout << "constructor called" << endl;
+        Type = name;
+        Servings = qty;
+        Ingridients = ing;
+    }
+
+    void Display()
+    {
+        cout << "Typye of coffee: " << Type << endl;
+        cout << "Number of Servings: " << Servings << endl;
+        for (string Ingridient : Ingridients)
+        {
+            cout << Ingridient << " ";
+        }
+        cout << endl;
+    }
+};
+int main()
+{
+
+    Coffee defaultCoffee({"coldcoffee",10,{"water,", "coffee-powder,", "suger,", "ice,", "milk-powder."}});//passing args to the parameters
+    defaultCoffee.Display();
+
+    Coffee copyCoffee(defaultCoffee);
+    copyCoffee.Display();
+    
+
+    return 0;
+}
--- C++/myClass.cpp
+++ C++/myClass.cpp
@@ -2,20 +2,79 @@
 using namespace std;
 
 class MyClass {
+private:
+    int myAge;          // Private members
+    string myName;
+    bool isEmployed;
+
 public:
+    // Public method
     void hello() {
         cout << "Hello, World!" << endl;
     }
-    int myAge;
-    string myName;
-    bool isEmployed;
+
+    // Setter for myAge
+    void setAge(int age) {
+        if (age >= 0) {
+            myAge = age;
+        } else {
+            cout << "Invalid age!" << endl;
+        }
+    }
+
+    // Getter for myAge
+    int getAge() {
+        return myAge;
+    }
+
+    // Setter for myName
+    void setName(string name) {
+        myName = name;
+    }
+
+    // Getter for myName
+    string getName() {
+        return myName;
+    }
+
+    // Setter for isEmployed
+    void setEmploymentStatus(bool status) {
+        isEmployed = status;
+    }
+
+    // Getter for isEmployed
+    bool getEmploymentStatus() {
+        return isEmployed;
+    }
 };
 
 int main() {
     MyClass obj;
+
     obj.hello();
-    cout << "Enter your age, name, and employment status: ";
-    cin >> obj.myAge >> obj.myName >> obj.isEmployed;
-    cout << obj.myAge << " " << obj.myName << " " << obj.isEmployed << endl;
+
+    // Using setters to assign values
+    int age;
+    string name;
+    bool employed;
+
+    cout << "Enter your age: ";
+    cin >> age;
+    obj.setAge(age);
+
+    cout << "Enter your name: ";
+    cin >> name;
+    obj.setName(name);
+
+    cout << "Are you employed? (1 for Yes, 0 for No): ";
+    cin >> employed;
+    obj.setEmploymentStatus(employed);
+
+    // Using getters to retrieve and display values
+    cout << "\nUser Details:" << endl;
+    cout << "Age: " << obj.getAge() << endl;
+    cout << "Name: " << obj.getName() << endl;
+    cout << "Employment Status: " << (obj.getEmploymentStatus() ? "Employed" : "Unemployed") << endl;
+
     return 0;
 }
