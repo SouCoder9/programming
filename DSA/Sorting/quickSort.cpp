775868deef4e573bed63bf67de6cfcbe073d9d2a
soucoder9 <soubhadra1234@gmail.com>
2025-01-30 15:13:08 +0530

    initial commit

---
+++ DSA/Sorting/quickSort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int partition(int arr[], int low, int high)
+{
+    int i = low - 1;
+    int pivot = arr[high];
+    for (int j = low; j < high; j++)
+    {
+        if (arr[j] < pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+int main()
+{
+    int n;
+    cout << "Enter the size of the array : ";
+    cin >> n;
+    int array[n];
+    for (int i = 0; i < n; i++){
+        cout << "Enter the element at index " << i << " : ";
+        cin >> array[i];
+    }
+    quickSort(array, 0, n-1);
+    for (int i = 0; i < n; i++)
+    {
+        cout << array[i] << " ";
+    }
+
+    return 0;
+}
